a918c6282ce24ab1f7f5507dc9af8f608de69b40
SB-9 <128657954+Samuel-Burgess@users.noreply.github.com>
2023-05-15 10:05:24 +1200

    editinggggg

--- AS-2.7-2.8 Monster Cards indedv v0.12.py
+++ AS-2.7-2.8 Monster Cards indedv v0.12.py
@@ -97,23 +97,31 @@ def main_menu():
                                    "Print cards to console", "Exit"])
     if choice == "Add Cards":
         card_name = add_cards()  # Get the new card name from add_cards()
-        edit_card(card_name)  # Pass the card name to edit_card()
-        main_menu()  # Call the main menu again
+        if card_name is None:  # if cancelled, return to menu
+            main_menu()
+        else:
+            edit_card(card_name)  # Pass the card name to edit_card()
+            main_menu()  # Call the main menu again
     elif choice == "Search for cards":
         card_name = find_cards()
-        if eg.buttonbox(f"Do you want to edit {card_name} or do you want to delete it?", "Edit or delete",
-                        choices=["Edit", "delete"]) == "Edit":
-            edit_card(card_name)
+        if card_name is None:
+            main_menu()
         else:
-            del cards[card_name]
-        main_menu()
+            if eg.buttonbox(f"Do you want to edit {card_name} or do you want to delete it?", "Edit or delete",
+                            choices=["Edit", "delete"]) == "Edit":
+                edit_card(card_name)
+                main_menu()
+            else:
+                del cards[card_name]
+                main_menu()
+
     elif choice == "Delete cards":
         delete_card()
     elif choice == "Print cards to console":
         print_cards()
         main_menu()
     else:
-        eg.msgbox("Thank you for using my program! i hope it was helpful.")
+        eg.msgbox("Thank you for using my program! I hope it was helpful.", "Goodbye")
         save_y_n = eg.buttonbox("Save data?", "Save", choices=["Yes", "No"])
         if save_y_n == "Yes":
             with open('cards_dictionary.pickle', 'wb') as handle:
@@ -125,20 +133,22 @@ def main_menu():
 
 def add_cards():  # add cards function gets the user to create a monster card then returns that card
     global cards
-    card_name = None
-    while card_name is None or card_name.strip() == "":
-        card_name = eg.enterbox("Please enter the name of the card you are adding:", "Add Card")
-    new_card = {}  # create the new card's dictionary
-    stats = ["Strength", "Speed", "Stealth", "Cunning"]  # define the stats that will be given values
-    for item in stats:
-        while True:
-            value = eg.integerbox(f"Please enter the value for {card_name}'s {item}", "Enter stat value", upperbound=25,
-                                  lowerbound=1)  # go through each stat getting the user to enter a value
-            if value is not None:
-                break
-        new_card[item] = value  # add the stat and value to the dictionary
-    cards |= {card_name: new_card}  # add the new card to the main dictionary
-    return card_name
+    card_name = eg.enterbox("Please enter the name of the card you are adding:", "Add Card")
+    if card_name is None or card_name.strip() == "":
+        eg.msgbox("Cancelled card creation", "Cancelled card creation")
+    else:
+        new_card = {}  # create the new card's dictionary
+        stats = ["Strength", "Speed", "Stealth", "Cunning"]  # define the stats that will be given values
+        for item in stats:
+            while True:
+                value = eg.integerbox(f"Please enter the value for {card_name}'s {item}", "Enter stat value",
+                                      upperbound=25,
+                                      lowerbound=1)  # go through each stat getting the user to enter a value
+                if value is not None:
+                    break
+            new_card[item] = value  # add the stat and value to the dictionary
+        cards |= {card_name: new_card}  # add the new card to the main dictionary
+        return card_name
 
 
 def edit_card(card_name):
@@ -160,14 +170,14 @@ def edit_card(card_name):
             attribute = eg.choicebox("What stat will you edit?", "Edit Stat",
                                      choices=["Strength", "Speed", "Stealth", "Cunning"])
             if attribute is None:
-                eg.msgbox("Cancelled stat edit")
+                eg.msgbox("Cancelled stat edit", "Cancel")
             else:
                 card[attribute] = eg.integerbox(f"What will be {card_name}'s {attribute}?", "Assign Value",
                                                 upperbound=25, lowerbound=1)
 
         elif choice == "Change name":
             new_card_name = None
-            while new_card_name is None or new_card_name.strip() == "":
+            while new_card_name is None:
                 new_card_name = eg.enterbox("Please enter the new name for the card", "New Name")
                 if new_card_name == original_card_name:
                     eg.msgbox("The new name is the same as the current name. Please enter a different name.",
@@ -177,7 +187,7 @@ def edit_card(card_name):
                     eg.msgbox(f"There is already a card named {new_card_name}. Please enter a new name.",
                               "Card already exists")
                     new_card_name = None
-                elif new_card_name.strip() != "":
+                elif new_card_name is not None and new_card_name.strip() != '':
                     cards[new_card_name] = cards.pop(card_name)  # Update the card name in the dictionary
                     card_name = new_card_name  # Update the current card name variable
                 else:
@@ -188,30 +198,31 @@ def edit_card(card_name):
 
 
 def find_cards():
-    card_name = None
-    while card_name is None or card_name.strip() == "":
-        card_name = eg.enterbox("What card do you want to find", "Find Card")
+    while True:
+        card_name = eg.enterbox("What card do you want to find?", "Find Card")
         if card_name in cards:
             card = cards[card_name]
-            msg = f"The card you have found is {card_name}\n\n"  # create the message for the first dialogue box
+            msg = f"The card you have found is {card_name}\n\n"
             msg += f"Strength:\t {card['Strength']}\n"
             msg += f"Speed:   \t {card['Speed']}\n"
             msg += f"Stealth: \t {card['Stealth']}\n"
             msg += f"Cunning: \t {card['Cunning']}\n"
             eg.msgbox(msg, f"Found {card_name}")
             return card_name
+        elif card_name is None:
+            eg.msgbox("Cancelled search", "Cancelled search")
+            return None
         else:
-            eg.msgbox(f"There is no {card_name} card, please enter an existing card", "Error")
-        card_name = None
+            eg.msgbox(f"There is no {card_name} card. Please enter an existing card.", "Error")
 
 
 def delete_card():
-    card_name = eg.choicebox("Choose a card to delete:", "Delete card", choices=list(cards.keys()))
+    card_name = eg.choicebox("Choose a card to delete:", "Delete Card", choices=list(cards.keys()))
     if card_name is not None:
         del cards[card_name]
-        eg.msgbox(f"The {card_name} card has been deleted", "Card deleted")
+        eg.msgbox(f"The {card_name} card has been deleted.", "Card Deleted")
     else:
-        eg.msgbox("Cancelled delete", "Cancel")
+        eg.msgbox("Cancelled delete", "Cancelled delete")
     main_menu()
 
 
